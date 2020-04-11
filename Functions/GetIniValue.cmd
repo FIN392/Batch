1dca001d8acb52a684bb83e543030cfb539117bf
Fin392 <63017766+FIN392@users.noreply.github.com>
2020-04-11 12:27:45 +0200

    v0

--- Functions/GetIniValue.cmd
+++ Functions/GetIniValue.cmd
@@ -20,14 +20,18 @@ SETLOCAL
 	:: Section	= findstr /I /N /R /C:"^[ ,	]*\[SECTION*\][ ,	]*$"
 	:: Value    = findstr /I /N /R /C:"^[ ,	]*VALUE[ ,	]*\="
 
+	:: Does the file exist?
+	IF NOT EXIST %2 GOTO GetIniValue_ELSE_Exist
 
 
 
+		SET _TmpValue=XXXX
 
-
-
-
-	SET _TmpValue=XXXX
+	GOTO GetIniValue_ENDIF_Exist
+	:GetIniValue_ELSE_Exist
+		ECHO [91mERROR: File '%2' doesn't exist[0m
+		SET _TmpValue=*** ERROR ***
+	:GetIniValue_ENDIF_Exist
 
 ENDLOCAL & SET %1=%_TmpValue%
 GOTO :EOF
