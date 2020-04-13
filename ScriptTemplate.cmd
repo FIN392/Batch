7cc679937f008f7df9fd415e1bc3bb4e7346f95d
Fin392 <63017766+FIN392@users.noreply.github.com>
2020-04-14 00:37:30 +0200

    v0

--- ScriptTemplate.cmd
+++ ScriptTemplate.cmd
@@ -1,7 +1,7 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
 ::! Syntax:
-::!     Script_Template.cmd [ /ON ^| /OFF ] [/X] [/F:{filename}]
+::!     Script_Template.cmd [/ON | /OFF] [/X] [/F:{filename}]
 ::!
 ::! Description:
 ::!     Here should go a long description of how the script works and the
@@ -31,7 +31,9 @@
 ::                 - Security vulnerabilities.
 ::  
 :Main
-@ECHO OFF & SETLOCAL
+@ECHO OFF
+SETLOCAL
+SET Err=0
 
 :: /?
 ECHO " %* " | find " /? " > NUL && (
@@ -41,20 +43,56 @@ ECHO " %* " | find " /? " > NUL && (
 
 
 
-	ECHO *
-	ECHO * YOU CODE IS HERE...
-	ECHO *
 
-	ECHO *
-	ECHO * Error handling
-	ECHO *
- 	TYPE "ThisFileDoNotExist.HopeSo" 2> NUL || (ECHO [91mERROR 999: Boom!!![0m&EXIT /B 999)
+	REM *
+	REM * YOU CODE IS HERE...
+	REM *
+
+	REM *
+	REM * Error handling
+	REM *
+ 	REM TYPE "ThisFileDoNotExist.HopeSo" 2> NUL || (
+	REM 	 ECHO [91mERROR %ERRORLEVEL%: Boom!!![0m
+	REM 	 GOTO :End_of_script
+	REM )
+
+	REM *
+	REM * Calling a function
+	REM *
+	CALL :FunctionName %1 %2 %3 %4 
+	ECHO ERRORLEVEL=%ERRORLEVEL%
+
+	echo %QWE%
 
 
-
-:: End of script
 :End_of_script
-ENDLOCAL & EXIT /B 0
+TITLE
+ENDLOCAL & EXIT /B %Err%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+:: FunctionName [/ON | /OFF] [/X] [/F:{filename}]
+::
+:: Here should go a long description of how the script works and the
+:: explanation of each of the parameters. The line length should be adjusted to
+:: 80 characters.
+::
+:FunctionName
+SETLOCAL
+
+	REM *
+	REM * YOU CODE IS HERE...
+	REM *
+	SET /A _Value=%2 + %3
+	REM *
+
+SET Err=999
+
+
+ENDLOCAL & SET %1=%_Value%& EXIT /B %Err%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 ::
 :: EOF
 ::
