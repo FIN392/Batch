1250c9bfc33e35c1268debaa0966d2c34eb3907b
Fin392 <63017766+FIN392@users.noreply.github.com>
2020-04-13 21:31:19 +0200

    v0

--- ScriptTemplate.cmd
+++ ScriptTemplate.cmd
@@ -1,41 +1,41 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
-::@ Syntax:
-::@     Script_Template.cmd [ /ON ^| /OFF ] [/X] [/F:{filename}]
-::@
-::@ Description:
-::@     Here should go a long description of how the script works and the
-::@     explanation of each of the parameters. The line length should be
-::@     adjusted to 80 characters.
-::@
-::@ Requirements:
-::@     '(none)' or the list of required files, configurations (e.g., registry
-::@     values) or systems (e.g., Internet access).
+::! Syntax:
+::!     Script_Template.cmd [ /ON ^| /OFF ] [/X] [/F:{filename}]
+::!
+::! Description:
+::!     Here should go a long description of how the script works and the
+::!     explanation of each of the parameters. The line length should be
+::!     adjusted to 80 characters.
+::!
+::! Requirements:
+::!     '(none)' or the list of required files, configurations (e.g., registry
+::!     values) or systems (e.g., Internet access).
 ::
-:: Author: fin392@gmail.com
+::  Author: fin392@gmail.com
 ::
-:: License: https://opensource.org/licenses/MIT
+::  License: https://opensource.org/licenses/MIT
 ::
-:: Additional information: https://github.com/FIN392/Batch
+::  Additional information: https://github.com/FIN392/Batch
 ::
-:: Change Log
-:: ----------
-:: 2020/04/13 - fin392@gmail.com - Initial version
-:: 2525/01/01 - fin392@gmail.com - Here should go a long description of change
-::              done, keeping de line length below 80 and including:
-::                - New features added.
-::                - Changes in existing functionality.
-::                - Soon-to-be removed features.
-::                - Now removed features.
-::                - Any bug fixes.
-::                - Security vulnerabilities.
+::  Change Log
+::  ----------
+::  2020/04/13 - fin392@gmail.com - Initial version
+::  2525/01/01 - fin392@gmail.com - Here should go a long description of change
+::               done, keeping de line length below 80 and including:
+::                 - New features added.
+::                 - Changes in existing functionality.
+::                 - Soon-to-be removed features.
+::                 - Now removed features.
+::                 - Any bug fixes.
+::                 - Security vulnerabilities.
 ::  
 :Main
 @ECHO OFF & SETLOCAL
 
 :: /?
 ECHO " %* " | find " /? " > NUL && (
-	ECHO. & FOR /F "tokens=1* delims=@" %%A IN ('findstr /B /C:"::@" "%~f0"') DO (ECHO.   %%B)
+	ECHO. & FOR /F "tokens=1* delims=!" %%A IN ('findstr /B /C:"::!" "%~f0"') DO (ECHO.   %%B)
 	GOTO :End_of_script
 )
 
