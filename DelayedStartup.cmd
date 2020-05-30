69459a74f29357a0d242fa2370c0a197998586f5
Fin392 <fin392@gmail.com>
2020-05-30 08:39:05 +0200

    v0

--- DelayedStartup.cmd
+++ DelayedStartup.cmd
@@ -1,30 +1,23 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
-::! Syntax:
-::!     DelayedStartup.cmd
+::! Syntax: DelayedStartup.cmd
 ::!
-::! Description:
-::!     xxx.
+::! Description: Launch several program waiting some seconds before each.
 ::!
-::! Requirements:
-::!     (none)
-::
-::  Author: fin392@gmail.com
-::
-::  License: https://opensource.org/licenses/MIT
-::
-::  Additional information: https://github.com/FIN392/Batch
+::! Requirements:(none)
+::!
+::! Author: jose.fernandez@abbott.com
 ::
 ::  Change Log
 ::  ----------
-::  2020/05/29 - fin392@gmail.com - Initial version
-::  2525/01/01 - fin392@gmail.com - Future version:
-::                 - New features added.
-::                 - Changes in existing functionality.
-::                 - Soon-to-be removed features.
-::                 - Now removed features.
-::                 - Any bug fixes.
-::                 - Security vulnerabilities.
+::  2020/05/29 - jose.fernandez@abbott.com - Initial version
+::  2020/05/29 - jose.fernandez@abbott.com - Version 1.0
+:: 		- New features added: none.
+:: 		- Changes in existing functionality: none.
+:: 		- Soon-to-be removed features: none.
+:: 		- Now removed features: none.
+:: 		- Any bug fixes: none.
+:: 		- Security vulnerabilities: none.
 ::  
 :Main
 @ECHO OFF & SETLOCAL & SET "_Error=0"
@@ -35,22 +28,15 @@ ECHO " %* " | find " /? " > NUL && (
 	GOTO :End_of_script
 )
 
+	MODE CON COLS=120 LINES=10
+	COLOR 0A
+	TITLE %~n0
 
-	REM CALL :LaunchProgram SKYPE   "C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe" .                                                  10
-	REM CALL :LaunchProgram EQUINOX "C:\Program Files (x86)\Avaya\Avaya Equinox\Avaya Equinox.exe"   "C:\Program Files (x86)\Avaya\Avaya Equinox\"      10
-	REM CALL :LaunchProgram WEBEX   "C:\Program Files (x86)\Webex\Webex\Applications\ptoneclk.exe"   "C:\Program Files (x86)\Webex\Webex\Applications\" 10
-	REM CALL :LaunchProgram TEAMS   "C:\Users\FERNAJL\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe" "C:\Users\FERNAJL\AppData\Local\Microsoft\Teams" 10
-	REM CALL :LaunchProgram ONEDRIVE "C:\Users\FERNAJL\AppData\Local\Microsoft\OneDrive\OneDrive.exe" . 10
-
-	REM *
-	REM * Calling a function
-	REM *
-	CALL :LaunchProgram TEST 2 3  4 || ECHO Function failed
-	SET "_Error=%ERRORLEVEL%"
-	ECHO Returned error=%_Error%
-	ECHO.
-
-
+	CALL :LaunchProgram 10 SKYPE    .                                                  "C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe"
+	CALL :LaunchProgram 10 EQUINOX  "C:\Program Files (x86)\Avaya\Avaya Equinox\"      "C:\Program Files (x86)\Avaya\Avaya Equinox\Avaya Equinox.exe"
+	CALL :LaunchProgram 10 WEBEX    "C:\Program Files (x86)\Webex\Webex\Applications\" "C:\Program Files (x86)\Webex\Webex\Applications\ptoneclk.exe"
+	CALL :LaunchProgram 10 TEAMS    "C:\Users\FERNAJL\AppData\Local\Microsoft\Teams"   "C:\Users\FERNAJL\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe"
+	CALL :LaunchProgram 10 ONEDRIVE .                                                  "C:\Users\FERNAJL\AppData\Local\Microsoft\OneDrive\OneDrive.exe"
 
 :End_of_script
 ENDLOCAL & EXIT /B %_Error%
@@ -58,18 +44,31 @@ ENDLOCAL & EXIT /B %_Error%
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
-:: LaunchProgram {Aplication Name} {Program} {Path} {TimeOut}
+:: LaunchProgram {TimeOut} {Title} {Path} {Program} [{Parameters}...]
 ::
-:: xxx.
+:: Wait some seconds and then launch the program.
+:: 		{TimeOut}:                   Time in seeconds to wait
+:: 		{Title}:                     Title of program to launch
+:: 		{Path}:                      Path where start the program
+:: 		{Program} [{Parameters}...]: Program and parameters
 ::
 :LaunchProgram
 SETLOCAL & SET "_Error=0"
 
-	ECHO %1
-	PUSHD %3
-echo %2
+	SET _TimeOut=%1
+	SET _Title=%2
+	SET _Path=%3
+	SET _ProgramParam=
+	:LoopParam
+		SET _ProgramParam=%_ProgramParam% %4
+		SHIFT
+	IF NOT '%4'=='' GOTO LoopParam
+
+	TIMEOUT /T %_TimeOut%
+	ECHO Launching %_Title%...
+	PUSHD %_Path%
+	START "%_Title%" %_ProgramParam%
 	POPD
-	TIMEOUT /T %4
 
 	SET "_Error=0"
 
