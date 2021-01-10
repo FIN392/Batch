81d0509c77b0eed6d712d6d0dd49ab9e9e7c4c92
Fin392 <fin392@gmail.com>
2021-01-10 22:11:59 +0100

    Update StrLowercase.cmd

--- Functions/StrLowercase.cmd
+++ Functions/StrLowercase.cmd
@@ -9,6 +9,10 @@
 	CALL :StrLowercase LOWER "This is "another" test"
 	ECHO Uppercase=[%LOWER%]
 
+	CHCP 65001 > NUL
+	CALL :StrLowercase LOWER "'Árbol' is tree in Spanish (ESPAÑOL)"
+	ECHO Uppercase=[%LOWER%]
+
 :End_of_script
 ENDLOCAL & EXIT /B %_Error%
 
@@ -22,7 +26,8 @@ SETLOCAL ENABLEDELAYEDEXPANSION & SET "_Error=0"
 
 	SET "_Return=%~2"
 	FOR %%a IN ( "A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m"
-				 "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z" ) DO (
+				 "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z"
+				 "Á=á" "É=é" "Í=í" "Ó=ó" "Ú=ú" "Ü=ü" "Ñ=ñ" ) DO (
 		FOR /F "usebackq tokens=1,2 delims==" %%b IN ('%%~a') DO (
 			SET _Return=!_Return:%%b=%%c!
 		)
