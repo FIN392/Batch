1e6ff3d67a8dcb8c5003bc2fd48fbbe6b779d478
Fin392 <fin392@gmail.com>
2021-01-10 22:12:11 +0100

    Update StrUppercase.cmd

--- Functions/StrUppercase.cmd
+++ Functions/StrUppercase.cmd
@@ -9,6 +9,10 @@
 	CALL :StrUppercase UP "This is "another" test"
 	ECHO Uppercase=[%UP%]
 
+	CHCP 65001 > NUL
+	CALL :StrUppercase LOWER "'árbol' is tree in Spanish (Español)"
+	ECHO Uppercase=[%LOWER%]
+
 :End_of_script
 ENDLOCAL & EXIT /B %_Error%
 
@@ -22,7 +26,8 @@ SETLOCAL ENABLEDELAYEDEXPANSION & SET "_Error=0"
 
 	SET "_Return=%~2"
 	FOR %%a IN ( "a=A" "b=B" "c=C" "d=D" "e=E" "f=F" "g=G" "h=H" "i=I" "j=J" "k=K" "l=L" "m=M"
-				 "n=N" "o=O" "p=P" "q=Q" "r=R" "s=S" "t=T" "u=U" "v=V" "w=W" "x=X" "y=Y" "z=Z" ) DO (
+				 "n=N" "o=O" "p=P" "q=Q" "r=R" "s=S" "t=T" "u=U" "v=V" "w=W" "x=X" "y=Y" "z=Z"
+				 "á=Á" "é=É" "í=Í" "ó=Ó" "ú=Ú" "ü=Ü" "ñ=Ñ" ) DO (
 		FOR /F "usebackq tokens=1,2 delims==" %%b IN ('%%~a') DO (
 			SET _Return=!_Return:%%b=%%c!
 		)
