01f7d7400f9859bc5e94c486ec9854ea6d4921b6
Fin392 <fin392@gmail.com>
2021-01-11 01:27:14 +0100

    Update StrLowercase.cmd

--- Functions/StrLowercase.cmd
+++ Functions/StrLowercase.cmd
@@ -9,7 +9,6 @@
 	CALL :StrLowercase LOWER "This is "another" test"
 	ECHO Uppercase=[%LOWER%]
 
-	CHCP 65001 > NUL
 	CALL :StrLowercase LOWER "'Árbol' is tree in Spanish (ESPAÑOL)"
 	ECHO Uppercase=[%LOWER%]
 
@@ -25,14 +24,42 @@ ENDLOCAL & EXIT /B %_Error%
 SETLOCAL ENABLEDELAYEDEXPANSION & SET "_Error=0"
 
 	SET "_Return=%~2"
-	FOR %%a IN ( "A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m"
-				 "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z"
-				 "Á=á" "É=é" "Í=í" "Ó=ó" "Ú=ú" "Ü=ü" "Ñ=ñ" ) DO (
-		FOR /F "usebackq tokens=1,2 delims==" %%b IN ('%%~a') DO (
-			SET _Return=!_Return:%%b=%%c!
-		)
-	)
 
-:End_StrLowercase
+	SET _Return=%_Return:A=a%
+	SET _Return=%_Return:B=b%
+	SET _Return=%_Return:C=c%
+	SET _Return=%_Return:D=d%
+	SET _Return=%_Return:E=e%
+	SET _Return=%_Return:F=f%
+	SET _Return=%_Return:G=g%
+	SET _Return=%_Return:H=h%
+	SET _Return=%_Return:I=i%
+	SET _Return=%_Return:J=j%
+	SET _Return=%_Return:K=k%
+	SET _Return=%_Return:L=l%
+	SET _Return=%_Return:M=m%
+	SET _Return=%_Return:N=n%
+	SET _Return=%_Return:O=o%
+	SET _Return=%_Return:P=p%
+	SET _Return=%_Return:Q=q%
+	SET _Return=%_Return:R=r%
+	SET _Return=%_Return:S=s%
+	SET _Return=%_Return:T=t%
+	SET _Return=%_Return:U=u%
+	SET _Return=%_Return:V=v%
+	SET _Return=%_Return:W=w%
+	SET _Return=%_Return:X=x%
+	SET _Return=%_Return:Y=y%
+	SET _Return=%_Return:Z=z%
+
+	:: Add your language-specific characters here
+	SET _Return=%_Return:Á=á%
+	SET _Return=%_Return:É=é%
+	SET _Return=%_Return:Í=í%
+	SET _Return=%_Return:Ó=ó%
+	SET _Return=%_Return:Ú=ú%
+	SET _Return=%_Return:Ü=ü%
+	SET _Return=%_Return:Ñ=ñ%
+
 ENDLOCAL & SET "%~1=%_Return%" & EXIT /B %_Error%
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
