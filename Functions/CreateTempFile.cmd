d9708afec674d9557bf5ca92e63b83ee470ce965
Fin392 <fin392@gmail.com>
2021-01-12 01:15:36 +0100

    Update CreateTempFile.cmd

--- Functions/CreateTempFile.cmd
+++ Functions/CreateTempFile.cmd
@@ -1,22 +1,22 @@
 :: Example of using the function
-:Main
-@ECHO OFF & SETLOCAL & SET "_Error=0"
+@ECHO OFF & SETLOCAL
 
-	CALL :CreateTempFile _MyTempFile || ECHO Temp file creation failed
-	SET "_Err=%ERRORLEVEL%"
+	CALL :CreateTempFile MyTempFile || (
+		ECHO *** Temp file creation failed
+		EXIT /B 1
+	)
 
-	ECHO Temp file is: [%_MyTempFile%]
+	ECHO Temp file is: [%MyTempFile%]
 
 	:: Remember to delete temp files at the end
-	IF EXIST "%_MyTempFile%" DEL "%_MyTempFile%"
+	IF EXIST "%MyTempFile%" DEL "%MyTempFile%" > NUL 2>&1
 
-:End_of_script
-ENDLOCAL & EXIT /B %_Err%
+ENDLOCAL & EXIT /B 0
 
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
-:: Create a empty file on folder %TEMP% with name '~{yyyymmddhhmmss}.tmp'.
+:: Create a empty file on folder %TEMP% with name '~yyyymmddhhmmss.tmp'.
 ::
 :: Remember to delete the temporary files at the end of your script.
 ::
@@ -27,18 +27,13 @@ ENDLOCAL & EXIT /B %_Err%
 SETLOCAL
 
 	:CreateTempFile_Loop
-
-		FOR /F %%t IN ('wmic OS GET LocalDateTime /VALUE ^| find "="') DO SET _%%t
-
-	IF EXIST "%TEMP%\~%_LocalDateTime:~0,14%.tmp" GOTO CreateTempFile_Loop
+		FOR /F "tokens=2 delims=.=" %%t IN ('wmic OS GET LocalDateTime /VALUE ^| find "="') DO SET "_File=%TEMP%\~%%t.tmp"
 	
-	(TYPE NUL > "%TEMP%\~%_LocalDateTime:~0,14%.tmp") 2> NUL && (
-		SET "_File=%TEMP%\~%_LocalDateTime:~0,14%.tmp"
-		SET "_Error=0"
-	) || (
-		SET "_File="
-		SET "_Error=1"
-	)
+	IF EXIST "%_File%" GOTO CreateTempFile_Loop
+	
+	(TYPE NUL > "c%_File%") 2> NUL
+
+	IF EXIST "%_File%" ( SET "_Error=0" ) ELSE ( SET "_File=" & SET "_Error=1" )
 
 ENDLOCAL & SET "%~1=%_File%" & EXIT /B %_Error%
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
