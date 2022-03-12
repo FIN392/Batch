87a370f3f6dfa0d40608336a6ad8b49d9f920c8f
Fin392 <fin392@gmail.com>
2022-03-12 22:39:46 +0100

    .

--- How to/How to.cmd
+++ How to/How to.cmd
@@ -49,31 +49,3 @@ FOR /F "tokens=1-30 delims=," %%a IN ('TYPE "%TEMP%\~.tmp"') DO (
 DEL "%TEMP%\~.tmp" > NUL 2>&1
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-:: Look for path name longer than 255 characters
-FOR /F "Tokens=*" %%a IN ('DIR C:\ /B /S /A') DO (
-	SET PathName=%%a
-	IF NOT "!PathName:~255,1!"=="" ECHO Extra long name: "%%a"
-)
-
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-:: View % of progress
-SET /A "intTotalElements=23"
-FOR /L %%i IN (0, 1, %intTotalElements%) DO (
-	SET /A "intPercentage=(%%i*100)/%intTotalElements%"
-    ECHO|SET /P="%%i of %intTotalElements% (!intPercentage!%%)"
-	
-	REM *
-	REM * YOUR WORK GOES HERE
-	REM *
-	
-    TIMEOUT /T 1 > NUL 2>&1
-)
-ECHO.
-
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-
-
-
