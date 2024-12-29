c6ccaa4e9d6da961bd8f011af374bcd073f337e2
Fin392 <fin392@gmail.com>
2024-12-29 18:50:46 +0100

    👌

--- Templates/Basic script.cmd
+++ Templates/Basic script.cmd
@@ -1,8 +1,15 @@
 @ECHO OFF
 SETLOCAL
 PUSHD "%~dp0"
+    >> "%~dpn0.log" ECHO %DATE% %TIME% *** START
+
 
     :: Your code goes here
+    >> "%~dpn0.log" ECHO Log what your code does
+    :: Your code goes here
 
+
+    >> "%~dpn0.log" ECHO %DATE% %TIME% *** STOP
+    >> "%~dpn0.log" ECHO.
 POPD
 ENDLOCAL
