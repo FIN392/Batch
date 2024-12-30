57274509edca24dfef023cf444dddf0860064660
Fin392 <fin392@gmail.com>
2024-12-30 01:28:57 +0100

    👌

--- Templates/Basic script.cmd
+++ Templates/Basic script.cmd
@@ -1,9 +1,9 @@
-@ECHO OFF
-SETLOCAL
+@ECHO OFF & SETLOCAL
 PUSHD "%~dp0"
     >> "%~dpn0.log" ECHO %DATE% %TIME% *** START
 
-
+    :: Your code goes here
+    CALL :FunctionTemplate
     :: Your code goes here
     >> "%~dpn0.log" ECHO Log what your code does
     :: Your code goes here
@@ -12,4 +12,11 @@ PUSHD "%~dp0"
     >> "%~dpn0.log" ECHO %DATE% %TIME% *** STOP
     >> "%~dpn0.log" ECHO.
 POPD
-ENDLOCAL
+ENDLOCAL & EXIT /B 0
+
+:FunctionTemplate
+SETLOCAL
+
+    :: Your code goes here
+
+ENDLOCAL & EXIT /B 0
